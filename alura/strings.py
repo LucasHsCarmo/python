2055cd4fb8908536a163e2ef95a8b1f474d78a89
LucasHsCarmo <lucascarmo10@hotmail.com>
2020-01-15 23:31:42 -0300

    Trabalhando com string

---
+++ alura/strings.py
@@ -0,0 +1,11 @@
+"""
+Interpolação {:02d}
+               {} -> Aonde vou passar a variavel
+               :  -> Define a quantidade de decimais
+               0  -> Primeiro numerador (qual numero deverá aparecer no print)
+               2  -> Quantidade de casas que serão respeitados
+               d  -> Tipo da variável 
+"""         
+print("Data {:02d}/{:02d}/{:4d}".format(10,3,2020))
+
+print("R$: {:7.2f}".format(1234.8))
