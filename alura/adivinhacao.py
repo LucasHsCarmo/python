c561863fae4209e40f90be8e14bae758bd24e1f0
LucasHsCarmo <lucascarmo10@hotmail.com>
2020-01-19 23:04:22 -0300

    Jogo de adivinhação

--- alura/adivinhacao.py
+++ alura/adivinhacao.py
@@ -1,9 +1,26 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
-total_tentativas = 3
+numero_secreto = random.randint(1,100)
+total_tentativas = 0
+pontuacao = 1000
+
+print("\nQual o nível de dificuldade?")
+print("(1) - Fácil")
+print("(2) - Médio")
+print("(3) - Difícil")
+
+nivel = int(input("\nEscolha o seu nível: "))
+
+if(nivel == 1):
+    total_tentativas = 20
+elif(nivel == 2):
+    total_tentativas = 10
+else:
+    total_tentativas = 5
 
 for rodada in range(1, total_tentativas+1):
     print("Tentativa {} de {}".format(rodada,total_tentativas))
@@ -20,12 +37,14 @@ for rodada in range(1, total_tentativas+1):
     menor = chute < numero_secreto
 
     if(acertou):
-        print("Você acertou o chute\n")
+        print("Você acertou o chute e fez {} pontos!\n".format(pontuacao))
         break
     else:
         if (maior):
             print("Você errou! O seu chute foi maior que o número secreto\n")
         elif (menor):
             print("Você errou! O seu chute foi menor que o número secreto\n")
+        pontos_perdidos = abs(pontos_perdidos - chute)
+        pontuacao -= pontos_perdidos
 
 print("Fim de jogo")
--- alura/strings.py
+++ alura/strings.py
@@ -9,3 +9,5 @@ Interpolação {:02d}
 print("Data {:02d}/{:02d}/{:4d}".format(10,3,2020))
 
 print("R$: {:7.2f}".format(1234.8))
+
+#built in python são módulos que já são pré-definidos no python
