4b0a64f56348eabec7200b1acc5bcb4a711f5474
LucasHsCarmo <lucascarmo10@hotmail.com>
2020-02-08 11:30:08 -0300

    Continuando jogo forca

--- alura/forca.py
+++ alura/forca.py
@@ -4,6 +4,43 @@ def jogar():
     print("Bem vindo ao jogo da forca!")
     print("***************************")
 
+    palavra_secreta = "abacaxi".upper()
+    palpite = ['_' for letra in palavra_secreta] #Insere "_" para cada letra dentro da palavra_secreta
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(palpite)
+
+    #enquanto(True):
+    while(not enforcou and not acertou):
+
+        chute = input("\nInforme uma letra: ")
+        chute = chute.strip().upper() #Retirando os espaços em branco
+
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra): #Comparando letras maiusculas
+                    palpite[index] = letra #Posição correta da letra
+                index += 1
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(len(palpite)-erros))    
+
+        print(palpite)
+
+        if(erros == len(palpite)):
+            break
+        if("_" not in palpite):
+            break
+
+    if("_" not in palpite):
+        print("\nVocê ganhou!")
+    else:
+        print("\nVocê perdeu!")
+
     print("Fim de jogo")
 
 if(__name__ == "__main__"):
--- alura/strings.py
+++ alura/strings.py
@@ -6,8 +6,36 @@ Interpolação {:02d}
                2  -> Quantidade de casas que serão respeitados
                d  -> Tipo da variável 
 """         
-print("Data {:02d}/{:02d}/{:4d}".format(10,3,2020))
+#print("Data {:02d}/{:02d}/{:4d}".format(10,3,2020))
 
-print("R$: {:7.2f}".format(1234.8))
+#print("R$: {:7.2f}".format(1234.8))
 
 #built in python são módulos que já são pré-definidos no python
+
+frutas = ['Maça', 'Melância', 'Uva']
+
+fruta_buscada = input("Informe a fruta que está procurando: ")
+
+if(fruta_buscada in frutas):
+    print("Parabéns, você encontrou a fruta desejada.")
+else:
+    print("A fruta {} não consta no sistema".format(fruta_buscada))
+
+
+#List Comprehension. 
+lista = [1,2,3,4,5,6]
+
+quadrado = [n*n for n in lista]
+print(quadrado)
+
+"""
+inteiros = [1,2,3,4,5,6,7,8,9,10]
+pares = []
+for numero in inteiros:
+    if(numero % 2 == 0):
+        pares.append(numero)
+"""
+
+inteiros = [1,2,3,4,5,6,7,8,9,10]
+pares = [numero for numero in inteiros if numero % 2 == 0]
+print(pares)
